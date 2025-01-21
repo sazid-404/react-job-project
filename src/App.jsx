d223cfa939989077892e67b466e1c62e4401e8c8
sazid-404 <sazzadulislamsazid@gmail.com>
2025-01-21 13:37:32 -0600

    creating new component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './component/Navbar'
 import Hero from './component/Hero'
 import HomeCards from './component/HomeCards'
 import JobListings from './component/jobListings'
+import ViewAllJobs from './component/ViewAllJobs'
 
 const App = () => {
   return (
@@ -11,17 +12,7 @@ const App = () => {
     <Hero/>
     <HomeCards/>
     <JobListings/>
-
-    
-    
-
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
+    <ViewAllJobs/>
     </>
   )
 }
